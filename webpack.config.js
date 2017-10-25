bf3569c7029ac2da9d66fea27c11356fbe21a5be
HaiTo <thewordr2.ver.xes@gmail.com>
2017-10-25 18:52:50 +0900

    configure webpack on serverless

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals')
+
+module.exports = {
+  entry: './handler.js',
+  target: 'node',
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.js',
+    ]
+  },
+  externals: [
+    nodeExternals(),
+  ]
+};
